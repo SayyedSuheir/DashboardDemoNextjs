67d0a1d9b43e075d2e0add64032f9ab42f27fef2
suheirelsayyed <sayyed.suheir.auce@gmail.com>
2025-12-01 21:47:25 +0200

    added to new repo

---
+++ thedashboardapp/src/app/page.js
@@ -0,0 +1,12 @@
+// app/page.js
+import Login from '../components/login/Login'; // default export, no curly braces
+
+
+export default function Home() {
+  return (
+    <main>
+      <Login />
+      
+    </main>
+  );
+}
