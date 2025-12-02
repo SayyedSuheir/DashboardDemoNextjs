7718aa22a4309b6dc06bfc564617e261c109e2d0
suheirelsayyed <sayyed.suheir.auce@gmail.com>
2025-12-02 15:41:02 +0200

    register

--- thedashboardapp/app/api/register/route.js
+++ thedashboardapp/app/api/register/route.js
@@ -1,1 +1,28 @@
 import Users from '@/models/Users';
+import connectDB from '@/app/lib/mongodb';
+import bcrypt from 'bcrypt';
+import { NextResponse } from 'next/server';
+
+export async function PoST(request){
+    try{
+        const {name,email,password} = await request.json();
+        const userExistense = await Users.findOne({email})
+        if(userExistense){
+            return NextResponse.json({error:"user email existed"})
+        }
+
+        const hashedpassword = await bcrypt.hash(password,10)
+
+        const newUser = new Users({
+            name,
+            email,
+            password: hashedpassword
+
+        })
+        await newUser.save();
+        return NextResponse.json({message: "new User registered",status:201})
+
+    }catch(err){
+        return NextResponse.json({error: "error in server", status:500});
+    }
+}
